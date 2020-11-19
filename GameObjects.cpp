ee3a2beb9dd46a831b28d76536aac7a0a58cdf4b
MSpecter1git config --global user.name Mspecter1git config --global user.name MSpecter1 <michaelhe61@gmail.com>
2020-11-19 05:59:38 -0800

    Added Gameobjects Class

---
+++ GameObjects.cpp
@@ -0,0 +1,6 @@
+#include "GameObjects.hpp"
+
+GameObjects::GameObjects()
+{
+
+}
---
+++ Pocket.hpp
@@ -0,0 +1,14 @@
+#ifndef _POCKET_HPP_
+#define _POCKET_HPP_
+
+#include "GameObjects.hpp"
+
+class Pocket: public GameObjects {
+        private:
+            int PlayerNum;
+            int Pieces;
+            int PlayerTurn;
+        public:
+            
+};
+#endif
