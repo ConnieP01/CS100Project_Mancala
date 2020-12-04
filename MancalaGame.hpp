288e8ea6763efefa3512056f1882bfc01588144f
ConnieP01 <57119043+ConnieP01@users.noreply.github.com>
2020-12-03 22:16:07 -0800

    Update MancalaGame.hpp

--- MancalaGame.hpp
+++ MancalaGame.hpp
@@ -1,19 +1,26 @@
 #ifndef MANCALAGAME_HPP
 #define MANCALAGAME_HPP
 
+#include "BoardManager.hpp"
+#include "GameObjects.hpp"
 //#include composite
 //#include visitor
 
+class BoardManager;
+
 class MancalaGame {
+    private:
+        int option;
+        int totalPieces;
+        int quitGame;
+        int score;
+        //MancalaGame* game = new MancalaGame();
+
     public:
         MancalaGame();
         void MainMenu();
         void BeginGame();
         void Winner();
-        
-        int option;
-        bool quitGame;
-        int oneScore;
-        int twoScore;
 };
 #endif
+
