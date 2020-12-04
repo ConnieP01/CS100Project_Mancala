34420fef0c60bca788d60e8048dca07cd182217b
ConnieP01 <57119043+ConnieP01@users.noreply.github.com>
2020-12-03 22:16:38 -0800

    Update MancalaGame.cpp

--- MancalaGame.cpp
+++ MancalaGame.cpp
@@ -1,13 +1,14 @@
 #include "MancalaGame.hpp"
-#include "BoardManager.cpp"
+#include "BoardManager.hpp"
+#include "GameObjects.hpp"
+
 //#include visitor
 #include <iostream>
 
 using namespace std;
 
-void MancalaGame::MancalaGame() {
-    BoardManager game = new BoardManager();
-
+MancalaGame::MancalaGame() {
+    BoardManager* newGame = new BoardManager();
 }
 void MancalaGame::MainMenu() {
     quitGame = true;
@@ -18,7 +19,7 @@ void MancalaGame::MainMenu() {
         
     //How to play:
     cout << "Players begin by placing an equal number of beads in each of the pockets or pits except for" << endl;
-    cout << "the pits at the end of the board. Then, the first player picks up all the seeds from one pit" << endll
+    cout << "the pits at the end of the board. Then, the first player picks up all the seeds from one pit" << endl;
     cout << "from their side of the boardand deposits one seed on each following pit, except for the opponent's " << endl; 
     cout << "capture pits, which are located at the end of the board. After depositing all the seeds, if the last" << endl;
     cout << "pit the seed was placed in is empty, it is the second player's turn. Else, the player can pick up " << endl;
@@ -36,8 +37,7 @@ void MancalaGame::MainMenu() {
         if (option == 1) {
            //call BeginGame()
             cout << "Get ready!" << endl;
-
-
+            BeginGame();
         }
         else if (option == 2) {
             quitGame = true;
@@ -51,30 +51,39 @@ void MancalaGame::MainMenu() {
 }
 
 void MancalaGame::BeginGame() {
+    score = 0;
+    BoardManager* game = new BoardManager();
 
-    //call MainMenu
-
-    while () {
-        //call BoardGame
-            //visitor
-            //scores
+    while (score < 1) {
+        game->DisplayBoard();
+        score = game->TallyWinner();
     }
 
-    //when game over, call Winner() to get winner screen
-
+    //game->Winner();
 }
 
 void MancalaGame::Winner() {
+    quitGame = false;
+
     cout << "Game Over!" << endl;
 
     //Winner Screen
-    if (oneScore > twoScore) {
-        cout << "Player 1 wins!" << endl;
+    if (score == 1) {
+        cout << "Player 1 won!" << endl;
     }
     else {
-        cout << "Player 2 wins!" << endl;
+        cout << "Player 2 won!" << endl;
     }
 
-    //Play again? -> Main Menu
-    //Quit game?
+    cout << "Type 1 to play again." << endl;
+    cin >> option;
+
+    while (option != 1) {
+        cout << "Invalid input." << endl;
+        cin >> option;
+    }
+
+    if (option == 1) {
+        BeginGame();
+    }
 }
