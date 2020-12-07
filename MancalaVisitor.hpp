465f2c39817715429cc0ec71dafc0d6bf12d565d
jyjeachae <jchae007@ucr.edu>
2020-12-06 21:35:53 -0800

    Add renamed visitor file

--- MancalaVisitor.hpp
+++ MancalaVisitor.hpp
@@ -22,9 +22,20 @@ class MancalaVisitor {
 		virtual void VisitPocket(Pocket* p) = 0;
 		virtual void VisitBank(Bank* b) = 0;
 */
-		void VisitPocket(Pocket* p){ p->AddPieces(1);}
+		void VisitPocket(Pocket* p){
+			 p->AddPieces(1);
+			/*
+ 			If the last piece you drop is in an empty pocket on your side, you capture that piece and any pieces in the pocket directly
+  			opposite.
+  			if(p->GetPieces() == 1){
+				p->SetPieces(0);
+				addBank++;
+				start-> 
+  			*/
+		}
         	void VisitBank(Bank* b){b->AddPieces(1);}
-
+	private:
+		int addBank = 0;
 };
 
 #endif // __VISITOR_HPP__ 
