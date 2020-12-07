08c9849cdfefe4659c61e508187597805dcbc93a
MSpecter1git config --global user.name Mspecter1git config --global user.name MSpecter1 <michaelhe61@gmail.com>
2020-12-06 18:39:52 -0800

    Updated bm and test cases

--- Pocket.hpp
+++ Pocket.hpp
@@ -17,6 +17,8 @@ class Pocket: public GameObjects {
                 //INSERT VISITOR CODE HERE
             //}
 
+            void AddPieces(int num){ Pieces+=num; } //add a number of pieces
+            void SetPieces(int num){ Pieces = num; } //set number of pieces
             int GetPlayerNum()
             {
                 //cout<<"POCKET: ";
