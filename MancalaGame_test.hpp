1ff4213d17cbab03cbdc55c4589d037aa6be503f
jyjeachae <jchae007@ucr.edu>
2020-12-07 18:24:52 -0800

    Add test cases for visitor

--- MancalaGame_test.hpp
+++ MancalaGame_test.hpp
@@ -5,7 +5,31 @@
 
 #include "MancalaGame.cpp"
 //#include "BoardManager.cpp"
-
+TEST(InGameVisitorTest, AddPocket) {
+        BoardManager * gameTest = new BoardManager();
+        MancalaVisitor* vTest;
+        gameTest->GetObject(1,4)->Accept(vTest);
+        int hand = gameTest->GetNumPieces(1,4);
+        EXPECT_EQ(hand, 5);
+}
+TEST(InGameVisitorTest, AddBank) {
+        BoardManager * gameTest = new BoardManager();
+        MancalaVisitor* vTest;
+        gameTest->GetBank1()->Accept(vTest);
+        int hand = gameTest->GetBank1()->GetPieces();
+        EXPECT_EQ(hand, 1);
+}
+TEST(InGameVisitorTest, AddPocket0) {
+        BoardManager * gameTest = new BoardManager();
+        MancalaVisitor* vTest;
+        gameTest->GetObject(0,3)->Accept(vTest);
+        int hand = gameTest->GetNumPieces(0,3);
+        gameTest->GetObject(0,3)->SetPieces(0);
+        gameTest->GetObject(0,3)->Accept(vTest);
+        int newhand = gameTest->GetNumPieces(0,3);
+        EXPECT_EQ(hand, 5);
+        EXPECT_EQ(newhand, 1);
+}
 TEST(MainMenu, OptionOne) {
 //   MancalaGame* test = new MancalaGame();
    	 MancalaGame* game = MancalaGame::getInstance();
--- MancalaVisitor.hpp
+++ MancalaVisitor.hpp
@@ -3,29 +3,52 @@
 
 #include <iostream> 
 #include "GameObjects.hpp"
-#include "BoardManager.hpp"
-#include "MancalaGame.hpp"
+//#include "MancalaGame.hpp"
+//#include "BoardManager.hpp"
 
 using namespace std;
 
-class BoardManager;
-class MancalaGame;
+
+
 
 class MancalaVisitor {
 	public: 
 		 MancalaVisitor() {;}
-		void VisitPocket(GameObjects* p); /*{
+		void VisitPocket(GameObjects* p) {
 			 p->AddPieces(1);
 			
- 			If the last piece you drop is in an empty pocket on your side, you capture that piece and any pieces in the pocket directly
-  			opposite.
-  			if(p->GetPieces() == 1){
-				p->SetPieces(0);
-				addBank++;
-				start-> 
-  			
-		}*/
-        void VisitBank(GameObjects* b)/*{b->AddPieces(1);i}*/;
+/* 			MancalaGame* game = game->getInstance();
+                        int count = game->thisObject()->getCount();
+                        cout<<"Count: " <<count<<endl;
+                        cout<<"Pieces: "<<p->GetPieces()<<endl;
+                        if((p->GetPieces() == 1) && (count == 0)){
+                                int row = game->getRow();
+                                cout<<"Row: "<<row<<endl;
+                                int opposite;
+                                int col = game->getCol();
+                                cout<<"Column: "<<col<<endl;
+                                p->SetPieces(0);
+                                if(row == 1){
+                                        opposite=0;
+                                }
+                                else if(row == 0){
+                                        opposite=1;
+                                }
+                                BoardManager* game1 = new BoardManager();
+                                game1 = game->getObject();
+                                int hand = game1->GetNumPieces(opposite,col) + 1;
+                                game1->GetObject(opposite,col)->SetPieces(0);
+
+                                if(row == 0) {
+                                        game1->GetBank2()->AddPieces(hand);
+                                 }
+                                else{
+                                        game1->GetBank1()->AddPieces(hand);
+                                }
+			}
+*/		
+		}
+        void VisitBank(GameObjects* b){b->AddPieces(1);}
 	private:
 		int addBank = 0;
 };
