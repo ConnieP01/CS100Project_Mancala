7c6a5122fcf85360a073b9ffd2adcf633b8dd44d
jyjeachae <jchae007@ucr.edu>
2020-12-07 17:30:09 -0800

    Remove extra test file

--- unit_test.cpp
+++ unit_test.cpp
@@ -3,7 +3,7 @@
 #include "BoardManager_Test.hpp"
 #include "GameObjects_test.hpp"
 #include "MancalaGame_test.hpp"
-#include "MancalaVisitorTest.hpp"
+//#include "MancalaVisitorTest.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
