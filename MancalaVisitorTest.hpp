dc25005c5f0d936d9f649089673ce96441bbb704
jyjeachae <jchae007@ucr.edu>
2020-12-07 17:11:02 -0800

    Add test file

---
+++ MancalaVisitorTest.hpp
@@ -0,0 +1,21 @@
+#ifndef _MANCALAVISITOR_TEST_HPP
+#define _MANCALAVISITOR_TEST_HPP
+
+#include "gtest/gtest.h"
+
+#include <iostream>
+#include "BoardManager.hpp"
+
+using namespace std;
+
+TEST(MancalaVisitorTest, AddPocket) {
+        BoardManager * gameTest = new BoardManager();
+        gameTest->SetStartPieces();
+        MancalaVisitor* vTest;
+        gameTest->GetObject(1,4)->Accept(vTest);
+        int hand = gameTest->GetNumPieces(1,4);
+        EXPECT_EQ(hand, 5);
+}
+
+
+#endif
