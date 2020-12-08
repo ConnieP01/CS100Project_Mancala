2011171669abeb7cb43edf9732fb1ead94e29a0f
ConnieP01 <cpak1099@gmail.com>
2020-12-08 08:02:55 -0800

    quick update

--- unit_test.cpp
+++ unit_test.cpp
@@ -3,7 +3,6 @@
 #include "BoardManager_Test.hpp"
 #include "GameObjects_test.hpp"
 #include "MancalaGame_test.hpp"
-//#include "MancalaVisitorTest.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
